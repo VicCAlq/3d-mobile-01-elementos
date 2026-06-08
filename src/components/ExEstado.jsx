f5c4af9c131dcd6c63d53aa031baef23b313069f
keanorives <thiago.maia@nave.org.br>
2026-06-08 16:39:11 -0300

    sA

---
+++ src/components/ExEstado.jsx
@@ -0,0 +1,10 @@
+const estilo = StyleSheet.create({
+    view:{
+            padding: "10px",
+            height: "120px",
+            width: "90vw",
+            backgroundColor: "purple",
+            display: "flex",
+            flexDirection: "row",
+    }
+} );
