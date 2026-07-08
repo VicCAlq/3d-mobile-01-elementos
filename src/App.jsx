2cc8d7f6bb7b8f7a33531762bb28bb99958c5eb9
thiacstaa <anthoniely.costa@nave.org.br>
2026-07-08 09:41:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import root from './styles/main';
 import Divisoria from './components/Divisoria';
 import Paragrafo from './components/Paragrafo';
 import Botao from './components/Botao';
-
+//
 export default function App() {
   return (
     <View style={styles.container}>
