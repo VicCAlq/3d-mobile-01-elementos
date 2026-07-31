d5a802e259fab585ba13886e52d4b46a27c58478
hyuga64 <pedro.franca@nave.org.br>
2026-07-31 14:21:13 -0300

    Update AtvBotaoAcaoExterna.jsx

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -1,5 +1,16 @@
-/*
-[ATIVIDADE BOTÃO COM AÇÃO EXTERNA]
-
+@@ -4,3 +4,15 @@
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+
+import { View, Pressable, Text, StyleSheet } from "react-native";
+
+export default function Botao(comando){
+    return(
+        <View>
+            <Text>Clique Abaixo</Text>
+            <Pressable onPress={() => comando()}>
+                <Text>Clique aqui</Text>
+            </Pressable>
+        </View>
+    )
+}
