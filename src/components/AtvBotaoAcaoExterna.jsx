49f7731fe4691ba2d328485b07c4707f6ea7d5fe
Luquita-chan <lucas.caio@nave.org.br>
2026-09-01 13:02:05 -0300

    Add AtvBotaoAcaoExterna component
    Create a button component that executes a command function.

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -3,3 +3,40 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+import root from '../styles/main';
+
+const estilo = StyleSheet.create({
+  botao: {
+    borderRadius: 5,
+    padding: root.spacings.paddingM,
+    backgroundColor: root.colors.primaria,
+  },
+
+  texto: {
+    color: root.colors.texto,
+    fontSize: 16,
+  },
+
+  caixa: {
+    margin: 10,
+    // falta colocar mais estilos aqui
+  }
+});
+
+export default function AtvBotaoAcaoExterna({ comando }) {
+  return (
+    <View style={estilo.caixa}>
+      <Text>Clique abaixo</Text>
+
+      <Pressable
+        style={estilo.botao}
+        onPress={comando}
+      >
+        <Text style={estilo.texto}>Clique aqui</Text>
+      </Pressable>
+
+      {/* colocar alguma coisa aqui depois */}
+    </View>
+  );
+}
