531d8298ab0a5cfa195f58a7b4bbc06535830f1f
Jeis0 <jason.nascimento@nave.org.br>
2026-09-01 00:34:29 -0300

    nero

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -3,6 +3,7 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+
 import { View, Text, Pressable } from "react-native";
 
 export default function AtvBotaoAcaoExterna({ comando }) {
