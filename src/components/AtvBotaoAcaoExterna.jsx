d188d58975d0619c60b00b613dfa28e1b296cc2e
harieladorecife <mariaantonia.lima@nave.org.br>
2026-09-01 14:25:16 -0300

    Update AtvBotaoAcaoExterna.jsx

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -4,26 +4,16 @@
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
 
-import { View, Pressable, Text, StyleSheet } from 'react-native';
-import root from '../styles/main';
+import { View, Text, Pressable } from "react-native";
 
-const estilo = StyleSheet.create({
-  botao: {
-    borderRadius: "5px",
-    padding: root.spacings.paddingM,
-    backgroundColor: root.colors.primaria,
-  },
-  texto: {
-    color: root.colors.texto,
-    fontSize: "16px",
-  }
-})
+export default function AtvBotaoAcaoExterna({ comando }) {
+  return (
+    <View>
+      <Text>Clique abaixo</Text>
 
-export default function AtvBotaoAcaoExterna({ comando })
- { return ( <View> <Text>Clique abaixo</Text>
- <Pressable style={estilo.botao}
-     onPress={comando} >
-         <Text style={estilo.texto}>Clique aqui</Text> 
-         </Pressable>
-          </View> );
-          }
+      <Pressable onPress={comando}>
+        <Text>Clique aqui</Text>
+      </Pressable>
+    </View>
+  );
+}
